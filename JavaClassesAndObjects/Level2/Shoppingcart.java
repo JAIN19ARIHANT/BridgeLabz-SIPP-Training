1bb6f32392102bdf54eaee0bf6bb97a1def37c9b
JAIN19ARIHANT <2013sj19nov@gmail.com>
2025-06-24 09:09:36 +0530

    [Arihant Jain] : Add Level2 of JavaClassesAndObjects

--- JavaClassesAndObjects/Level2/Shoppingcart.java
+++ JavaClassesAndObjects/Level2/Shoppingcart.java
@@ -8,7 +8,7 @@ class CartItem {
     int quantity;
 
     
-    CartItem(String itemName, double price, int quantity)
+    CartItem(String itemName, double price, int quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
