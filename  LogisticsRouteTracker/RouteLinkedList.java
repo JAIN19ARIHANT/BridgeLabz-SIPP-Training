3ce4db42d150e5a609df4a35ab015ce7d3fbf27d
JAIN19ARIHANT <2013sj19nov@gmail.com>
2025-08-27 10:18:22 +0530

    [Arihant Jain]

---  LogisticsRouteTracker/RouteLinkedList.java
+++  LogisticsRouteTracker/RouteLinkedList.java
@@ -24,14 +24,58 @@ public class RouteLinkedList<T extends Checkpoint> {
         }
     }
 
-    public void removeCheckpoint(String cpId) {
+    public boolean removeCheckpoint(String cpId) {
+        if (head.Checkpoint.checkpointId.equals(cpId)) {
+            head = head.next;
+            System.out.println("Checkpoint : " + cpId + " removed");
+            return true;
+        }
+
         Node temp = head;
 
-        while (temp.Checkpoint.checkpointId.equals(cpId)) {
+        while (temp != null && !temp.Checkpoint.checkpointId.equals(cpId)) {
             temp = temp.next;
         }
 
+        if (temp == null) {
+            System.out.println("Checkpoint : " + cpId + " does not exists.");
+            return false;
+        }
+
+        temp.Checkpoint = temp.next.Checkpoint;
+        temp.next = temp.next.next;
+
+        System.out.println("Checkpoint : " + cpId + " removed");
+        return true;
+    }
+
+    public T findCheckpoint(String cpId) {
+        if (head.Checkpoint.checkpointId.equals(cpId)) {
+            return head.Checkpoint;
+        }
+
+        Node temp = head;
+
+        while (temp != null && !temp.Checkpoint.checkpointId.equals(cpId)) {
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            System.out.println("Checkpoint : " + cpId + " does not exists.");
+            return null;
+        }
+
+        temp.Checkpoint = temp.next.Checkpoint;
+        temp.next = temp.next.next;
+
+        System.out.println("Checkpoint : " + cpId + " removed");
+
+        return temp.Checkpoint;
+    }
+
+    public double totalDistance() {
         
     }
+
 
 }
